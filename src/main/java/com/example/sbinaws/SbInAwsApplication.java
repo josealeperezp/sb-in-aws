fe68b41f2322508f1ce3d9aef21c197ec3e8ef42
josealeperezp <josealeperezp@gmail.com>
2022-01-08 11:18:34 +0100

    first commit

---
+++ src/main/java/com/example/sbinaws/SbInAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbinaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbInAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbInAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sbinaws/SbInAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbinaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbInAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
